e4a6360e4a4fd87d62d032a78a55ad04db24d591
Pratiksha Sontakke <86586806+pratikshasontakke77@users.noreply.github.com>
2022-01-04 14:53:00 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,8 @@
+CREATE TABLE studentData(Name varchar(30), Roll_No integer, Address varchar(40), Mobile_Number bigint primary key NOT NULL, PAN_Number varchar(10));
+
+INSERT INTO studentData values('Pratiksha', 20111062, 'Aurangabad', 7700000002, 'PRAT200566');
+INSERT INTO studentData values('Krutika', 20111011, 'Pune', 9979799999, 'KRUT200544');
+INSERT INTO studentData VALUES('Pooja', 20111066, 'Mumbai', 9123456789, 'POO200446');
+
+INSERT INTO studentData values('Sahil', 20111014, 'Aagra', 921111111111, 'SAHI200236');
+INSERT INTO studentData values('Kailas', 20111023, 'Delhi', 9234567890, 'KAIL200112');
